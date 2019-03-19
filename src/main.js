0f182618578cfdd4e1ab71cbe925cd1771148756
Young Liu <youngliu92@gmail.com>
2019-03-18 20:55:40 -0700

    add jquery and bootstrap.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import $ from 'jquery';
 import { pingPong } from './ping-pong';
 
 $(document).ready(function() {
