6692d8f44d8eb82eb3155abfaa2d112ba3d72847
Young Liu <youngliu92@gmail.com>
2019-03-18 22:22:42 -0700

    add reusable object.

--- spec/ping-pong-spec.js
+++ spec/ping-pong-spec.js
@@ -2,8 +2,13 @@ import { PingPongBall } from "./../src/ping-pong.js"
 
 describe('Ping Pong Ball', function() {
 
+  var reusablePingPong;
+
+  beforeEach(function(){
+    reusablePingPong = new PingPongBall(5);
+  });
+
   it('should test whether a ping pong ball is created with the correct size', function() {
-    var ppb = new PingPongBall(5);
-    expect(ppb.radius).toEqual(5);
+    expect(reusablePingPong.radius).toEqual(5);
   });
 });
